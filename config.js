3136f80527cbf00e28982ffff1d29eef552b140a
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2023-09-19 20:48:46 +0200

    to-do addition

--- config.js
+++ config.js
@@ -93,3 +93,17 @@ document.title = `${title}`;
 
 const userTitle = document.getElementById("userTitle");
 userTitle.textContent = username;
+
+fetch('https://raw.githubusercontent.com/choccymilku/choccy-newer-and-improved/main/todo.md')
+    .then(response => response.text())
+    .then(text => {
+        var todo = text.split('\n');
+        var todo_list = document.getElementById('todo_list');
+        todo.forEach((item) => {
+            if (item.trim() !== '') {
+                var todo_item = document.createElement('li');
+                todo_item.textContent = item;
+                todo_list.appendChild(todo_item);
+            }
+        });
+    });
