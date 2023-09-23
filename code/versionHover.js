680a89f7763848a3d62438fe2521ccb63b051efb
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2023-09-23 20:47:11 +0200

    y

--- code/versionHover.js
+++ code/versionHover.js
@@ -4,7 +4,7 @@
   const versionHover = document.getElementById('version_container');
 
   // Function to toggle the visibility of the profile_hover div
-  function toggleProfileHover() {
+  function toggleVersionHover() {
     if (versionHover.style.display === 'block') {
         versionHover.style.display = 'none';
     } else {
@@ -15,23 +15,23 @@
   // Event listener to show or hide profile_hover when clicking on pfp
   pfp2.addEventListener('click', function (event) {
     event.stopPropagation(); // Prevent event bubbling to the document click listener
-    toggleProfileHover();
+    toggleVersionHover();
   });
 
   // Event listener to hide profile_hover when clicking outside the profile or profile_hover
   document.addEventListener('click', function (event) {
     const target = event.target;
     if (target !== profile && target !== versionHover && !profile.contains(target)) {
-      hideProfileHover();
+      hideVersionHover();
     }
   });
 
   // Function to hide the profile_hover div
-  function hideProfileHover() {
+  function hideVersionHover() {
     versionHover.style.display = 'none';
   }
 
   // Function to show the profile_hover div
-  function showProfileHover() {
+  function showVersionHover() {
     versionHover.style.display = 'block';
   }
