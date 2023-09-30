d4c12dc997f2456829bf7d86805228b758111019
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2023-09-30 15:13:33 +0200

    Update gallery.js

--- code/apis/gallery.js
+++ code/apis/gallery.js
@@ -93,14 +93,18 @@ setInterval(function() {
   }
   
   function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("gallery_image_full");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    try {
+        let i;
+        let slides = document.getElementsByClassName("gallery_image_full");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    } catch (error) {
     }
-    slides[slideIndex-1].style.display = "block";
-  }
+}
+
 
   
