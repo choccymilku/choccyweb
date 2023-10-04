2531a3986df25f08d2a2392344fced3a082eec86
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2023-10-04 17:08:48 +0200

    Update discord.js

--- code/apis/discord.js
+++ code/apis/discord.js
@@ -3,7 +3,7 @@ let connected = false;
 let reconnectTimeout;
 
 function connect() {
-  ws = new WebSocket('wss://lanyard.choccymilk.uk/socket');
+  ws = new WebSocket('wss://lapi.choccymilk.uk/socket');
 
   ws.addEventListener('open', () => {
     ws.send(JSON.stringify({
